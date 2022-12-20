dd0f68abfbf45646f52cf7d900749aa92648a7c6
Jonathan Frey <jonathanpaulfrey@gmail.com>
2022-12-20 14:32:37 +0100

    add plot_iterates

--- nosnoc/__init__.py
+++ nosnoc/__init__.py
@@ -3,5 +3,5 @@ from .nosnoc_opts import NosnocOpts
 from .nosnoc_types import MpccMode, IRKSchemes, StepEquilibrationMode, CrossComplementarityMode, IrkRepresentation, PssMode, IrkRepresentation, HomotopyUpdateRule, InitializationStrategy
 from .helpers import NosnocSimLooper
 from .utils import casadi_length, print_casadi_vector
-from .plot_utils import plot_timings, latexify_plot
+from .plot_utils import plot_timings, plot_iterates, latexify_plot
 from .rk_utils import rk4
