813e1ba2426e67ce744a5612baeada96211f5339
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-01-11 14:11:11 +0100

    add p_values and docstrings to NosnocSimLooper

--- nosnoc/helpers.py
+++ nosnoc/helpers.py
@@ -1,14 +1,29 @@
+from typing import Optional
+
 import numpy as np
 from .nosnoc import NosnocSolver
 
 
 class NosnocSimLooper:
 
-    def __init__(self, solver: NosnocSolver, x0: np.ndarray, Nsim: int):
+    def __init__(self, solver: NosnocSolver, x0: np.ndarray, Nsim: int, p_values: Optional[np.ndarray]=None):
+        """
+        :param solver: NosnocSolver to be called in a loop
+        :param x0: np.ndarray: initial state
+        :param Nsim: int: number of simulation steps
+        :param: p_values: Optional np.ndarray of shape (Nsim, n_p_glob), parameter values p_glob are updated at each simulation step accordingly.
+        """
         # check that NosnocSolver solves a pure simulation problem.
         if not solver.problem.is_sim_problem():
             raise Exception("NosnocSimLooper can only be used with pure simulation problem")
 
+        # p values
+        self.p_values = p_values
+        if self.p_values is not None:
+            if self.p_values.shape != (Nsim, solver.problem.model.dims.n_p_glob):
+                raise ValueError("p_values should have shape (Nsim, n_p_glob).")
+
+        # create
         self.solver = solver
         self.Nsim = Nsim
 
@@ -25,7 +40,11 @@ class NosnocSimLooper:
 
     def run(self) -> None:
         for i in range(self.Nsim):
+            # set values
             self.solver.set("x", self.xcurrent)
+            if self.p_values is not None:
+                self.solver.set("p_global", self.p_values[i, :])
+            # solve
             results = self.solver.solve()
             # collect
             self.X_sim += results["x_list"]
