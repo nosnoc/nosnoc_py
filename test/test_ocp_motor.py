ede92b5f7b6329ae2ac77ebcc94dbd92ff023d08
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-01-12 12:07:44 +0100

    dont test DIRECT step_equilibration in motor test

--- test/test_ocp_motor.py
+++ test/test_ocp_motor.py
@@ -17,18 +17,19 @@ def test_loop():
 
     for step_equilibration in nosnoc.StepEquilibrationMode:
         for pss_mode in nosnoc.PssMode:
-            opts = get_default_options()
-            opts.step_equilibration = step_equilibration
-            opts.pss_mode = pss_mode
-            # opts.print_level = 0
+            if step_equilibration != nosnoc.StepEquilibrationMode.DIRECT:
+                opts = get_default_options()
+                opts.step_equilibration = step_equilibration
+                opts.pss_mode = pss_mode
+                # opts.print_level = 0
 
-            # print(f"test setting: {opts}")
-            results = solve_ocp(opts)
+                # print(f"test setting: {opts}")
+                results = solve_ocp(opts)
 
-            x_traj = results["x_traj"]
+                x_traj = results["x_traj"]
 
-            assert np.allclose(x_traj[0], X0, atol=1e-4)
-            assert np.allclose(x_traj[-1], X_TARGET, atol=1e-4)
+                assert np.allclose(x_traj[0], X0, atol=1e-4)
+                assert np.allclose(x_traj[-1], X_TARGET, atol=1e-4)
 
 
 if __name__ == "__main__":
