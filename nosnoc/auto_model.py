a83b08afde09b6c284901d89618c225b9dba3e58
Anton Pozharskiy <apozharski@gmail.com>
2023-02-23 09:48:53 +0100

    nonlinear reformulation

--- nosnoc/auto_model.py
+++ nosnoc/auto_model.py
@@ -4,16 +4,22 @@ from itertools import product
 
 
 class NosnocAutoModel:
-    """
+    r"""
     An interface to automatically generate a NosnocModel given:
     - x: symbolic state vector
     - f_nonsmooth_ode: symbolic vector field of the nonsmooth ODE
-    Outputs the switching functions c as well as either:
+    Outputs the switching functions c as well as either (Not yet implemented):
     - F:
     - S:
     or:
     - alpha:
     - f:
+
+    Currently supported nonsmooth functions:
+    - :math: `\mathrm{sign}(\cdot)`
+    - :math: `\mathrm{max}(\cdot,\cdot)`
+    - :math: `\mathrm{min}(\cdot,\cdot)`
+    - TODO: more nonsmooth functions!
     """
 
     def __init__(self,
@@ -27,7 +33,27 @@ class NosnocAutoModel:
         self.f = []
         self.alpha = []
 
-    def rebuild(self, node: ca.SX):
+    def reformulate(self):
+        if self._detect_nonlinearities():
+            self._reformulate_nonlin()
+            return self.c, self.alpha
+        else:
+            self._reformulate_linear()
+            return self.c, self.S
+
+    def _reformulate_linear(self):
+        # TODO actually implement linear reformulation
+        pass
+
+    def _reformulate_nonlin(self):
+        self._rebuild_nonlin(self.f_nonsmooth_ode)
+
+    def _detect_nonlinearities(self):
+        # TODO actually implement a check for nonlinearity
+        return True
+
+    # TODO type output
+    def _rebuild_linear(self, node: ca.SX):
         if node.n_dep() == 0:
             # elementary variable
             return [node]
@@ -58,3 +84,60 @@ class NosnocAutoModel:
                 l_children = self.rebuild(node.dep(0))
                 r_children = self.rebuild(node.dep(1))
                 return [ca.SX.binary(node.op(), lc, rc) for lc, rc in product(l_children, r_children)]
+
+    def _rebuild_nonlin(self, node: ca.SX):
+        if node.n_dep() == 0:
+            # elementary variable
+            return [node]
+        elif node.n_dep() == 1:
+            # unary operator
+            if node.is_op(ca.OP_SIGN):
+                # TODO need to process multiple layers, i.e. a nonsmooth function of nonsmooth inputs. Not handled for now
+                #      but may be useful to detect at least.
+
+                # get c and alpha for this nonsmoothness
+                alpha, fresh = self._find_matching_expr_nonlin(node.dep(0))
+                if fresh:
+                    self.c.append(node.dep(0))
+
+                # calculate the equivalent expression for this node
+                eq_expr = 2*alpha - 1
+                return eq_expr
+            else:
+                # In this case do nothing
+                child = self._rebuild_nonlin(node.dep(0))
+                return ca.SX.unary(node.op(), child)
+            # TODO: add more switching functions
+        else:
+            if node.is_op(ca.OP_FMAX):
+                # get c and alpha for this nonsmoothness
+                alpha, fresh = self._find_matching_expr_nonlin(node.dep(0)-node.dep(1))
+                if fresh:
+                    self.c.append(node.dep(0)-node.dep(1))
+
+                # calculate the equivalent expression for this node
+                eq_expr = alpha*node.dep(0) + (1-alpha)*node.dep(1)
+                return eq_expr
+            elif node.is_op(ca.OP_FMIN):
+                # get c and alpha for this nonsmoothness
+                alpha, fresh = self._find_matching_expr_nonlin(node.dep(0)-node.dep(1))
+                if fresh:
+                    self.c.append(node.dep(0)-node.dep(1))
+
+                # calculate the equivalent expression for this node
+                eq_expr = (1-alpha)*node.dep(0) + alpha*node.dep(1)
+            else:
+                # binary operator
+                l_child = self._rebuild_nonlin(node.dep(0))
+                r_child = self._rebuild_nonlin(node.dep(1))
+                return ca.SX.binary(node.op(), l_child, r_child)
+
+    def _find_matching_expr_nonlin(self, expr: ca.SX):
+        found_c = None
+        found_alpha = None
+        # TODO: implement actual routine to find corresponding c
+        if found_c is not None:
+            return found_alpha, False
+        else:
+            # create a fresh alpha
+            return ca.SX.sym(f'alpha_{len(self.alpha)}'), True
