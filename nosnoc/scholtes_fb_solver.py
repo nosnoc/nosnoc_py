6f2c9c740932945d9d0086d2343d03418b36213f
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-03-06 19:19:10 +0100

    update scholtes_fb_solver

--- nosnoc/scholtes_fb_solver.py
+++ nosnoc/scholtes_fb_solver.py
@@ -12,12 +12,34 @@ from nosnoc.problem import NosnocModel, NosnocOcp
 from nosnoc.solver import NosnocSolverBase, get_results_from_primal_vector
 from nosnoc.nosnoc_opts import NosnocOpts
 from nosnoc.nosnoc_types import MpccMode, InitializationStrategy, CrossComplementarityMode, StepEquilibrationMode, PssMode, IrkRepresentation, HomotopyUpdateRule, ConstraintHandling
-from nosnoc.utils import casadi_vertcat_list, casadi_sum_list, print_casadi_vector, casadi_length, casadi_inf_norm_nan
+from nosnoc.utils import casadi_vertcat_list, casadi_sum_list, print_casadi_vector, casadi_length, casadi_inf_norm_nan, flatten
 from nosnoc.plot_utils import plot_matrix_and_qr, spy_magnitude_plot, spy_magnitude_plot_with_sign
 
 DEBUG = False
 
+
+def get_fraction_to_boundary(tau, current, delta):
+    # alpha_ref = 1
+    # n = len(delta)
+    # for i in range(n):
+    #     if delta[i] < 0.0:
+    #         alpha_ref = min(- tau * current[i] / delta[i], alpha_ref)
+
+    ix = np.where(delta<0)
+    if len(ix[0]) == 0:
+        return 1.0
+    else:
+        return min(np.min(-tau *current[ix]/delta[ix]), 1.0)
+
+    return alpha
+
 class NosnocCustomScholtesFBSolver(NosnocSolverBase):
+    def _setup_G(self) -> ca.SX:
+        prob = self.problem
+        ind_pos = sum([flatten(x) for x in [prob.ind_lam, prob.ind_theta, prob.ind_lambda_n, prob.ind_lambda_p, prob.ind_alpha, prob.ind_h]], [])
+        ind_pos.sort()
+        return prob.w[ind_pos]
+
     def __init__(self, opts: NosnocOpts, model: NosnocModel, ocp: Optional[NosnocOcp] = None):
         super().__init__(opts, model, ocp)
         prob = self.problem
@@ -35,17 +57,22 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
         n_comp = casadi_length(G1)
         n_H = casadi_length(H)
 
+        # G = (G1, G2) \geq 0
+        self.G = self._setup_G()
+        nG = casadi_length(self.G)
+
+
         # setup primal dual variables:
         lam_H = ca.SX.sym('lam_H', n_H)
         lam_comp = ca.SX.sym('lam_comp', n_comp)
         lam_pd = ca.vertcat(lam_H, lam_comp)
         self.lam_pd_0 = np.zeros((casadi_length(lam_pd),))
 
-        mu_G1 = ca.SX.sym('mu_G1', n_comp)
-        mu_G2 = ca.SX.sym('mu_G2', n_comp)
+        mu_G = ca.SX.sym('mu_G', nG)
         mu_s = ca.SX.sym('mu_s', n_comp)
-        mu_pd = ca.vertcat(mu_G1, mu_G2, mu_s)
+        mu_pd = ca.vertcat(mu_G, mu_s)
         self.mu_pd_0 = np.ones((casadi_length(mu_pd),))
+        self.n_mu = nG + n_comp
 
         slack = ca.SX.sym('slack', n_comp)
         w_pd = ca.vertcat(prob.w, slack, lam_pd, mu_pd)
@@ -54,9 +81,9 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
         self.nw = casadi_length(prob.w)
         self.nw_pd = casadi_length(w_pd)
 
+        ## KKT system
         # slack = - G1 * G2 + sigma
         self.slack0_expr = -ca.diag(G1) @ G2 + prob.sigma
-        self.slack0_fun = ca.Function('slack0_fun', [prob.w, prob.p], [self.slack0_expr])
 
         # barrier parameter
         tau = prob.tau
@@ -66,44 +93,58 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
         stationarity_w = ca.jacobian(prob.cost, prob.w).T \
             + ca.jacobian(H, prob.w).T @ lam_H \
             + ca.jacobian(slacked_complementarity, prob.w).T @ lam_comp \
-             - ca.jacobian(G1, prob.w).T @ mu_G1 \
-             - ca.jacobian(G2, prob.w).T @ mu_G2 \
-             - ca.jacobian(slack, prob.w).T @ mu_s
+            - ca.jacobian(self.G, prob.w).T @ mu_G \
+            - ca.jacobian(slack, prob.w).T @ mu_s
 
         stationarity_s = ca.jacobian(slacked_complementarity, slack).T @ lam_comp \
              - ca.jacobian(slack, slack).T @ mu_s
 
-        kkt_eq_without_comp = ca.vertcat(stationarity_w,stationarity_s, H, slacked_complementarity)
+        kkt_eq_without_comp = ca.vertcat(stationarity_w, stationarity_s, H, slacked_complementarity)
+
         # treat IP complementarities:
-        #  (G1, mu_1), (G2, mu_2), (s, mu_s) via FISCHER_BURMEISTER
         kkt_comp = []
+        # for i in range(nG):
+        #     kkt_comp = ca.vertcat(kkt_comp, self.G[i] * mu_G[i] - tau)
+        # for i in range(n_comp):
+        #     kkt_comp = ca.vertcat(kkt_comp, slack[i] * mu_s[i] - tau)
+        for i in range(nG):
+            kkt_comp = ca.vertcat(kkt_comp, self.G[i] + mu_G[i] - ca.sqrt(self.G[i] ** 2 + mu_G[i] ** 2  + tau**2))
         for i in range(n_comp):
-            kkt_comp = ca.vertcat(kkt_comp, G1[i] + mu_G1[i] - ca.sqrt(G1[i]**2 + mu_G1[i]**2 + 2*tau))
-        for i in range(n_comp):
-            kkt_comp = ca.vertcat(kkt_comp, G2[i] + mu_G2[i] - ca.sqrt(G2[i]**2 + mu_G2[i]**2 + 2*tau))
-        for i in range(n_comp):
-            kkt_comp = ca.vertcat(kkt_comp, slack[i] + mu_s[i] - ca.sqrt(slack[i]**2 + mu_s[i]**2 + 2*tau))
+            kkt_comp = ca.vertcat(kkt_comp, slack[i] + mu_s[i] - ca.sqrt(slack[i] ** 2 + mu_s[i] ** 2 + tau**2))
 
         kkt_eq = ca.vertcat(kkt_eq_without_comp, kkt_comp)
 
         self.kkt_eq = kkt_eq
         kkt_eq_jac = ca.jacobian(kkt_eq, w_pd)
 
-        # regularize kkt_compl jacobian
-        compl_mat = kkt_eq_jac[-3*n_comp:, :]
-        indicator_mat = np.zeros(compl_mat.shape)
-        for i in range(3*n_comp):
-            for j in range(self.nw_pd):
-                indicator_mat[i, j] = float(not compl_mat[i, j].is_zero())
-                if not compl_mat[i, j].is_zero():
-                    kkt_eq_jac[-3*n_comp+i, j] += 1e-7
 
-        self.kkt_eq_jac_fun = ca.Function('kkt_eq_jac_fun', [w_pd, prob.p], [kkt_eq, kkt_eq_jac])
-        self.kkt_eq_fun = ca.Function('kkt_eq_fun', [w_pd, prob.p], [kkt_eq])
+        # hessian without mu * G
+        stationarity_w_no_muG = ca.jacobian(prob.cost, prob.w).T \
+            + ca.jacobian(H, prob.w).T @ lam_H \
+            + ca.jacobian(slacked_complementarity, prob.w).T @ lam_comp \
+            - ca.jacobian(self.G, prob.w).T @ mu_G
+            # - ca.jacobian(slack, prob.w).T @ mu_s
+        kkt_eq_jac[:self.nw, :] = ca.jacobian(stationarity_w_no_muG, w_pd)
+
+
+        # regularize kkt_compl jacobian?
+        JIT = False
+        casadi_function_opts = {}
+        if JIT:
+            casadi_function_opts = {"compiler": "shell", "jit": True, "jit_options": {"compiler": "gcc", "flags": ["-O3"]}}
+
+        # newton_matrix[:self.nw, :self.nw] += 1e-5 * scipy.sparse.eye(self.nw)
+        # kkt_eq_jac[:self.nw, :self.nw] += 1e-5 * ca.diag((self.nw*[1]))
+
+        self.kkt_eq_jac_fun = ca.Function('kkt_eq_jac_fun', [w_pd, prob.p], [kkt_eq, kkt_eq_jac], casadi_function_opts)
+        self.kkt_eq_fun = ca.Function('kkt_eq_fun', [w_pd, prob.p], [kkt_eq], casadi_function_opts)
+        self.G_fun = ca.Function('G_fun', [prob.w, prob.p], [self.G], casadi_function_opts)
+        self.slack0_fun = ca.Function('slack0_fun', [prob.w, prob.p], [self.slack0_expr], casadi_function_opts)
 
         self.n_comp = n_comp
         self.n_H = n_H
-        self.kkt_eq_offsets = [0, self.nw, self.nw+n_comp]  + [n_H+self.nw + i * n_comp for i in range(1, 5)]
+        kkt_block_sizes = [self.nw, n_comp, n_H, n_comp, nG, n_comp]
+        self.kkt_eq_offsets = [0]  + np.cumsum(kkt_block_sizes).tolist()
 
         print(f"created primal dual problem with {casadi_length(w_pd)} variables and {casadi_length(kkt_eq)} equations, {n_comp=}, {self.nw=}, {n_H=}")
 
@@ -123,6 +164,10 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
         step = np.linalg.solve(matrix, rhs)
         return step
 
+    def compute_step_sparse(self, matrix, rhs):
+        # naive
+        step = scipy.sparse.linalg.spsolve(matrix, rhs)
+        return step
 # Schur complement:
 # [ A, B,
 # C, D]
@@ -179,6 +224,15 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
 
         return step
 
+    def check_newton_matrix(self, matrix):
+        upper_left = matrix[:self.nw, :self.nw]
+        lower_right = matrix[-self.n_mu:, -self.n_mu:]
+        print(f"conditioning: upper left {np.linalg.cond(upper_left):.3e}, lower right {np.linalg.cond(lower_right):.3e}")
+        return
+
+    def get_mu(self, w_current):
+        return w_current[-self.n_mu:]
+
     def solve(self) -> dict:
         """
         Solves the NLP with the currently stored parameters.
@@ -196,12 +250,12 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
                 (prob.model.p_val_ctrl_stages.flatten(),
                  np.array([opts.sigma_0, tau_val]), lambda00, x0))
 
-        lamH0 = 0 * np.ones((self.n_H,))
+        lamH0 = 1.0 * np.ones((self.n_H,))
         lampd0 = np.concatenate((lamH0, np.ones(self.n_comp,)))
 
-        slack0 = self.slack0_fun(prob.w0, p_val).full().flatten()
-        # slack0 = np.zeros((self.n_comp,))
-        w_pd_0 = np.concatenate((prob.w0, slack0, lampd0, self.mu_pd_0))
+        # slack0 = self.slack0_fun(prob.w0, p_val).full().flatten()
+        slack0 = np.ones((self.n_comp,))
+        w_pd_0 = np.concatenate((prob.w0, lampd0, slack0, self.mu_pd_0))
 
         w_current = w_pd_0
 
@@ -211,11 +265,19 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
         cpu_time_nlp = n_iter_polish * [None]
         nlp_iter = n_iter_polish * [None]
         sigma_k = opts.sigma_0
+        # timers
+        t_la = 0.0
+        t_ls = 0.0
+        t_ca = 0.0
 
+        n_mu = self.n_mu
         # TODO: initialize duals ala Waechter2006, Sec. 3.6
         # if opts.fix_active_set_fe0 and opts.pss_mode == PssMode.STEWART:
+        if opts.print_level == 1:
+            print(f"sigma\t\t iter \t res \t min_steps \t min_mu \t max mu \t minG \t cond(A)")
 
-        max_gn_iter = 30
+        w_candidate = w_current.copy()
+        max_gn_iter = 150
         # homotopy loop
         for ii in range(opts.max_iter_homotopy):
             tau_val = sigma_k
@@ -224,40 +286,76 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
                  np.array([sigma_k, tau_val]), lambda00, x0))
 
             if opts.print_level > 1:
-                print("alpha \t step norm \t kkt res \t cond(A)")
+                # print("alpha \t alpha_mu \t step norm \t kkt res \t min mu \t min G")
+                print("alpha \t step norm \t kkt res \t min mu \t min G")
             t = time.time()
             alpha_min_counter = 0
 
             for gn_iter in range(max_gn_iter):
 
+                t0_ca = time.time()
                 kkt_val, jac_kkt_val = self.kkt_eq_jac_fun(w_current, p_val)
-                newton_matrix = jac_kkt_val.full()
+                newton_matrix = jac_kkt_val.sparse()
+                t_ca += time.time() - t0_ca
 
                 # cond = np.linalg.cond(newton_matrix)
-                # self.plot_newton_matrix(newton_matrix, title=f'newton matrix, cond(A) = {cond:.2e}', )
-                # regularize Lagrange Hessian
+                # self.plot_newton_matrix(newton_matrix, title=f'newton matrix, cond(A) = {cond:.2e}', fig_filename=f'newton_matrix_{ii}_{gn_iter}.pdf')
 
-                newton_matrix[:self.nw, :self.nw] += 1e-7 * np.eye(self.nw)
-                # self.plot_newton_matrix(newton_matrix, title=f'regularized matrix', )
+                # if DEBUG:
+                #     self.check_newton_matrix(newton_matrix)
+
+                # regularize Lagrange Hessian
+                # newton_matrix[-n_mu:, -n_mu:] += 1e-5 * np.eye(n_mu)
+                # newton_matrix[:self.nw, :self.nw] += 1e-5 * np.eye(self.nw)
+
+                t0_la = time.time()
+                # newton_matrix[-n_mu:, -n_mu:] += 1e-5 * scipy.sparse.eye(n_mu)
+                for i in range(n_mu):
+                    newton_matrix[-n_mu + i, -n_mu + i] += 1e-5 * np.sign(newton_matrix[-n_mu + i, -n_mu + i])
+                # newton_matrix[:self.nw, :self.nw] += 1e-5 * scipy.sparse.eye(self.nw)
+                # for i in range(self.nw):
+                #     newton_matrix[i, i] += 1e-5 * np.sign(newton_matrix[i, i])
                 rhs = - kkt_val.full().flatten()
 
-                # compute step
+
+                ## compute step
                 # step = self.compute_step_schur_np(newton_matrix, rhs)
                 # step = self.compute_step_schur_scipy(newton_matrix, rhs)
-                step = self.compute_step(newton_matrix, rhs)
+                # step = self.compute_step(newton_matrix, rhs)
+                step = self.compute_step_sparse(newton_matrix, rhs)
+                t_la += time.time() - t0_la
 
                 step_norm = np.linalg.norm(step)
                 nlp_res = casadi_inf_norm_nan(kkt_val)
                 if step_norm < sigma_k or nlp_res < sigma_k / 10:
                     break
 
+                ## LINE SEARCH
+                t0_ls = time.time()
+                # fraction to boundary
+                tau_min = .99 # .99 is IPOPT default
+                tau_j = max(tau_min, 1-tau_val)
+                # tau_j = tau_min # dont use so large tau for now
+
+                # # compute alpha_mu_k
+                # mu_current = self.get_mu(w_current)
+                # mu_step = self.get_mu(step)
+                # alpha_mu = get_fraction_to_boundary(tau_j, mu_current, mu_step)
+                # w_candidate[-n_mu:] = mu_current + alpha_mu * mu_step
+
+                # # fraction to boundary G1, G2 > 0
+                G_val = self.G_fun(w_current[:self.nw], p_val).full().flatten()
+                # G_delta_val = self.G_fun(step[:self.nw], p_val).full().flatten()
+                # alpha_k_max = get_fraction_to_boundary(tau_j, G_val, G_delta_val)
+
                 # line search:
                 alpha = 1.0
                 rho = 0.9
                 gamma = .3
-                alpha_min = 0.05
+                alpha_min = 0.01
                 while True:
-                    w_candidate = w_current + alpha * step
+                    # w_candidate[:-n_mu] = w_current[:-n_mu] + alpha * step[:-n_mu]
+                    w_candidate[:] = w_current[:] + alpha * step[:]
                     step_res_norm = casadi_inf_norm_nan(self.kkt_eq_fun(w_candidate, p_val))
                     if step_res_norm < (1-gamma*alpha) * nlp_res:
                         break
@@ -267,7 +365,22 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
                     else:
                         alpha *= rho
 
-                w_current = w_candidate
+                # if step_res_norm > nlp_res:
+                #     print(f"alpha = {alpha:.2e}, alpha_k_max = {alpha_k_max:.2e}, alpha_mu = {alpha_mu:.2e}")
+                #     print(f"residual increase from {nlp_res:.2e} to {step_res_norm:.2e}")
+                    # breakpoint()
+
+                # do step!
+                w_current = w_candidate.copy()
+                t_ls += time.time() - t0_ls
+
+
+                # G_val_new = self.G_fun(w_current[:self.nw], p_val).full().flatten()
+                # if any(G_val_new < 0):
+                #     print("got G_val_new < 0")
+                #     print(f"{alpha=}, {alpha_k_max=}")
+                #     print(f"{G_val_new=}")
+                #     breakpoint()
 
                 if DEBUG:
                     maxA = np.max(np.abs(newton_matrix))
@@ -275,12 +388,13 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
                     tmp = newton_matrix.copy()
                     tmp[tmp==0] = 1
                     minA = np.min(np.abs(tmp))
-                    print(f"{alpha:.3f} \t {step_norm:.2e} \t {nlp_res:.2e} \t {cond:.2e} \t {maxA:.2e} \t {minA:.2e}")
+                    print(f"{alpha:.3f} \t {alpha_mu:.3f} \t {step_norm:.2e} \t {nlp_res:.2e} \t {cond:.2e} \t {maxA:.2e} \t {minA:.2e}")
                     # self.plot_newton_matrix(newton_matrix, title=f'regularized matrix: sigma = {sigma_k:.2e} cond = {cond:.2e}',
                     #         fig_filename=f'newton_spy_reg_{prob.model.name}_{ii}_{gn_iter}.pdf'
                     #  )
                 elif opts.print_level > 1:
-                    print(f"{alpha:.3f} \t {step_norm:.2e} \t {nlp_res:.2e} \t")
+                    # print(f"{alpha:.3f} \t {alpha_mu:.3f} \t {step_norm:.2e} \t {nlp_res:.2e} \t {min_mu:.2e}\t {np.min(G_val):.2e}\t")
+                    print(f"{alpha:.3f} \t {step_norm:.2e} \t {nlp_res:.2e}\t")
 
             cpu_time_nlp[ii] = time.time() - t
 
@@ -292,8 +406,19 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
 
             complementarity_residual = prob.comp_res(w_current[:self.nw], p_val).full()[0][0]
             complementarity_stats[ii] = complementarity_residual
-            if opts.print_level:
+            if opts.print_level > 1:
                 print(f"sigma = {sigma_k:.2e}, iter {gn_iter}, res {nlp_res:.2e}, min_steps {alpha_min_counter}")
+            elif opts.print_level == 1:
+                min_mu = np.min(self.get_mu(w_current))
+                max_mu = np.max(self.get_mu(w_current))
+                # print(f"{sigma_k:.2e} \t {gn_iter} \t {nlp_res:.2e} \t {alpha_min_counter}\t {min_mu:.2e} \t {max_mu:.2e} \t {np.min(G_val):.2e}\t")
+                np_mat = newton_matrix.toarray()
+                condA = np.linalg.cond(np_mat)
+                print(f"{sigma_k:.2e} \t {gn_iter} \t {nlp_res:.2e} \t {alpha_min_counter}\t {min_mu:.2e} \t {max_mu:.2e}\t {np.min(G_val):.2e} \t{condA:.2e}\t")
+                if condA > 1e5:
+                    self.check_newton_matrix(np_mat)
+                    breakpoint()
+
 
             if complementarity_residual < opts.comp_tol:
                 break
@@ -325,7 +450,9 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
 
         sum_iter = sum([i for i in nlp_iter if i is not None])
         total_time = sum([i for i in cpu_time_nlp if i is not None])
-        print(f"total iterations {sum_iter}, CPU time {total_time:.3f}")
+        print(f"total iterations {sum_iter}, CPU time {total_time:.3f}: LA: {t_la:.3f} line search: {t_ls:.3f} casadi: {t_ca:.3f}")
+        if nlp_res > sigma_k:
+            breakpoint()
 
         # self.print_iterate(w_current)
         return results
@@ -350,12 +477,12 @@ class NosnocCustomScholtesFBSolver(NosnocSolverBase):
         plt.show()
 
     def add_scatter_spy_magnitude_plot(self, ax, fig, matrix):
-        # spy_magnitude_plot(matrix, ax=ax, fig=fig,
-        spy_magnitude_plot_with_sign(matrix, ax=ax, fig=fig,
-            xticklabels=[r'$w$', r'$\lambda_H$', r'$\lambda_{\mathrm{comp}}$', r'$s$', r'$\mu_1$', r'$\mu_2$', r'$\mu_s$'],
-            xticks = self.kkt_eq_offsets,
-            yticklabels= ['stat w', 'stat s', '$H$', 'slacked comp', 'comp $G_1$', 'comp $G_2$', 'comp $s$'],
-            yticks = self.kkt_eq_offsets
+        spy_magnitude_plot(matrix, ax=ax, fig=fig,
+        # spy_magnitude_plot_with_sign(matrix, ax=ax, fig=fig,
+            xticklabels=[r'$w$', r'$s$', r'$\lambda_H$', r'$\lambda_{\mathrm{comp}}$', r'$\mu_G$', r'$\mu_s$'],
+            xticks = self.kkt_eq_offsets[:-1],
+            yticklabels= ['stat w', 'stat s', '$H$', 'slacked comp', 'comp $G$', 'comp $s$'],
+            yticks = self.kkt_eq_offsets[:-1]
         )
         return
 
