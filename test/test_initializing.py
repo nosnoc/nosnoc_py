3536c53a7dc28750dc0ae439eb9a2810aef11db6
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-04-24 11:10:46 +0200

    fix test_initialization

--- test/test_initializing.py
+++ test/test_initializing.py
@@ -12,8 +12,14 @@ class TestInitialization(unittest.TestCase):
         global X0
         opts = get_default_options()
         opts.terminal_time = TERMINAL_TIME
+        opts.sigma_0 = 1e-1
+        opts.comp_tol = 1e-6
+
         opts2 = get_default_options()
         opts2.terminal_time = TERMINAL_TIME
+        opts2.sigma_0 = 1e-1
+        opts2.comp_tol = 1e-6
+
         [model, ocp] = get_sliding_mode_ocp_description()
         [model2, ocp2] = get_sliding_mode_ocp_description()
         u1_est = np.zeros((opts.N_stages, 1))
@@ -22,25 +28,25 @@ class TestInitialization(unittest.TestCase):
         u2_est = np.zeros((opts.N_stages, 1))
         u2_est[-1] = 0.5
         u2_est[-2] = -2.0
-        u = np.concatenate((u1_est, u2_est), axis=1)
+        u_guess = np.concatenate((u1_est, u2_est), axis=1)
         X0 = X0.reshape((1, -1))
         x_est = np.concatenate((X0 * 2 / 3, X0 * 1 / 3, np.zeros((opts.N_stages - 2, 4))))
 
         opts.initialization_strategy = nosnoc.InitializationStrategy.EXTERNAL
         solver = nosnoc.NosnocSolver(opts, model, ocp)
-        solver2 = nosnoc.NosnocSolver(opts2, model2, ocp2)
         solver.set("x", x_est)
         for i in range(opts.N_stages):
             self.assertTrue(
                 np.array_equal(solver.problem.w0[solver.problem.ind_x[i][0][0]], x_est[i, :]))
 
-        solver.set("u", u)
-        self.assertTrue(np.array_equal(solver.problem.w0[solver.problem.ind_u], u))
+        solver.set("u", u_guess)
+        self.assertTrue(np.array_equal(solver.problem.w0[solver.problem.ind_u], u_guess))
 
         print("Solve with explicit initialization")
         res_initialized = solver.solve()
 
         print("Solve without explicit initialization")
+        solver2 = nosnoc.NosnocSolver(opts2, model2, ocp2)
         res_uninitialized = solver2.solve()
         ipopt_iter_initialized = sum(res_initialized["nlp_iter"])
         ipopt_iter_uninitialized = sum(res_uninitialized["nlp_iter"])
