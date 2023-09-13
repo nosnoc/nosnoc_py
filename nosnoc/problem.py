cda3b48d02dad7b5bffd934f3002f98055e136c4
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-09-13 17:40:04 +0200

    initialize mu with 1.0 again

--- nosnoc/problem.py
+++ nosnoc/problem.py
@@ -305,7 +305,7 @@ class FiniteElement(FiniteElementBase):
                 for ij in range(dims.n_sys):
                     self.add_variable(ca.SX.sym(f'mu_{ctrl_idx}_{fe_idx}_{ii+1}_{ij+1}', 1),
                                       self.ind_mu, -np.inf * np.ones(1), np.inf * np.ones(1),
-                                      0.5 * np.ones(1), ii, ij)
+                                      1.0 * np.ones(1), ii, ij)
             elif opts.pss_mode == PssMode.STEP:
                 # add alpha
                 for ij in range(dims.n_sys):
