5fe581d8dd51284b97e357789c5754416b02d96c
Anton Pozharskiy <apozharski@gmail.com>
2026-04-29 16:56:36 +0200

    initial discrete_time_problem work

---
+++ nosnoc/discrete_time_problem/stewart.py
@@ -0,0 +1,30 @@
+from .base import Base
+from vdx_py.vector import *
+
+import casadi as ca
+import numpy as np
+
+class Stewart(Base):
+
+    def __init__(self, dcs, opts):
+        super().__init__(dcs, opts)
+
+    @override
+    def _create_variables(self):
+        """Create Optimization Variables"""
+        pass
+
+    @override
+    def _generate_direct_transcription_constraints(self):
+        """Create direct transcription constraints"""
+        pass
+
+    @override
+    def _generate_complementarity_constraints(self):
+        """Create complementarity constraints"""
+        pass
+
+    @override
+    def _generate_step_equilibration_constraints(self):
+        """Create step equilibration constraints"""
+        pass
