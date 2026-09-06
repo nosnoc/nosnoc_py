3e479ab8143d78b17478790e85bc2aecafe17f6f
Anton Pozharskiy <apozharski@gmail.com>
2026-09-06 10:31:57 +0200

    working smooth OcpSolver and added smooth cartpole example

--- src/nosnoc/model/__init__.py
+++ src/nosnoc/model/__init__.py
@@ -2,4 +2,5 @@ from .base import Base
 from .pss import Pss, PssDims
 from .heaviside import Heaviside, HeavisideModelDims
 from .cls import Cls, ClsDims
+from .dae import Dae, DaeDims
 # TODO(@anton) others
